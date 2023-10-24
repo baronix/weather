3387aca38af6a1fa294b0e6f37fd9e8419ed9c7e
baronix <42778871+baronix@users.noreply.github.com>
2023-10-24 13:32:54 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const WEATHERURL = `https://api.weatherapi.com/v1/current.json?key=ba035b8002f1446388a101110232410&q=Lisbon&aqi=no`;
+
 
 function App() {
+
+  const [weather,setWeather] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+        useEffect(() => {
+          axios.get(WEATHERURL)
+          .then(response => {
+            console.log(response);
+            setWeather(response.data);
+            setIsLoading(false);
+           }, error => {
+              console.log(error);
+            });
+          }, []);
+          console.log(weather);  
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+     <div className={ 
+      (weather.current.temp_c > 25) 
+      ?
+      "bg-[#FF0000] flex flex-col justify-center text-center h-screen items-center"
+      :
+      "bg-[#00a2ff] flex flex-col justify-center text-center h-screen items-center"
+      }>
+      <div >
+        {isLoading && <p className="Loader" style={{opacity: isLoading ? 2 : 0}}>Loading...</p>}
+        <h1 className="text-black text-[100px] font-mono">{weather.current.temp_c}ºC</h1>
+        <div className="flex items-center justify-center">
+          <h1 className="text-black text-xl">{weather.current.condition.text}</h1>
+          <img className="max-h-[10x]" src={weather.current.condition.icon} alt="Weather Condition"/>
+        </div>
+        <h1 className="text-black text-2xl">{weather.current.humidity} % humidity</h1>
+        <h1 className="text-black text-xl italic pt-4 font-mono">{weather.location.name}, {weather.location.country}</h1>
     </div>
+    </div> 
+    </>
   );
 }
 
