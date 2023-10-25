c66e2491c69528d313513f46866662f3240997fc
baronix <42778871+baronix@users.noreply.github.com>
2023-10-25 13:45:35 +0100

    bug fix

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -45,14 +45,9 @@ const WeatherApp = () => {
                     onChange={event => setLocation(event.target.value)}
                     onKeyPress={searchLocation}
                     placeholder='Pesquisar cidade'
-                    className='peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
-                    /* className='border-2 font-mono border-gray-300 bg-white h-10  rounded-lg text-sm text-center focus:outline-none' */
+                    className='border-2 font-mono border-gray-300 bg-white h-10  rounded-lg text-sm text-center focus:outline-none'
                     />
-                    <label
-                     for="floatingInput"
-                    class="pointer-events-none text-center absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                    >Pesquisar cidade</label
-                    >
+
                 </div>
                 <div className='top'>
                 <div className="flex flex-col items-center justify-center mt-3">
