28430ee722f18c7a37b6c071f3b13020ebcee31b
baronix <42778871+baronix@users.noreply.github.com>
2023-10-25 15:07:07 +0100

    Update WeatherApp.jsx

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -33,9 +33,9 @@ const WeatherApp = () => {
 
 
   return (
-    <div className={weather.current ? (weather.current.temp_c>25 ? "bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-500 to-yellow-300 h-screen flex items-center justify-center" : "bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500 h-[100vh] flex items-center justify-center overflow-auto ") : "h-screen flex items-center justify-center"}>
+    <div className={weather.current ? (weather.current.temp_c>25 ? "bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-500 to-yellow-300 h-screen flex items-center justify-center" : "bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500 h-[100vh] flex items-center justify-center overflow-auto ") : "h-screen flex items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 to-gray-600 bg-gradient-to-r"}>
         <div className='bg-white w-[500px] max-w[1240px] m-auto rounded-[25px] bg-opacity-70 text-black pt-10 pb-10 shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] overflow-y-auto animate-fade'>
-            <div className='flex flex-col items-center justify-center '>
+            <div className='flex flex-col items-center justify-center ease-in-out'>
                 <img src={logoImg} alt="" className='max-w-[250px] pb-5 animate-fade animate-delay-200'/>
                 <div className="relative mb-3">
                     <input 
@@ -51,7 +51,7 @@ const WeatherApp = () => {
                 </div>
                 <div className='top'>
                 <div className="flex flex-col items-center justify-center mt-3">
-                        {weather.location ? <p className='font-bold text-2xl font-mono animate-fade-up'>{weather.location.name}</p> : null}
+                        {weather.location ? <p className='font-bold text-2xl font-mono animate-fade-up'>{weather.location.name}, {weather.location.country}</p> : null}
                         {weather.location ? <p className='text-sm italic font-mono animate-fade-up'>{weather.location.localtime}</p> : null}
                     </div>
                     <div className='flex flex-col items-center mt-3'>
