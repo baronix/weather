2fa08d192d66e76e9e76e88acb2952e17449ba85
baronix <42778871+baronix@users.noreply.github.com>
2023-10-25 13:40:38 +0100

    Update WeatherApp.jsx

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -20,10 +20,11 @@ const WeatherApp = () => {
     const searchLocation = (event) => {
 
         if (event.key === 'Enter') {
-            axios.get(WEATHERURL).then((response)=> {
+            axios.get(WEATHERURL)
+            .then((response)=> {
             setWeather(response.data)/* 
-            console.log(response.data) */
-        })
+            console.log(response.data) */})
+            .catch((err) => { console.log(err)})
         setLocation('')
         }
 
@@ -32,19 +33,26 @@ const WeatherApp = () => {
 
 
   return (
-    <div className={weather.current ? (weather.current.temp_c>25 ? "bg-[#FAC898] h-screen flex items-center justify-center" : "bg-[#D8E9EC] h-screen flex items-center justify-center") : null}>
+    <div className={weather.current ? (weather.current.temp_c>25 ? "bg-[#FAC898] h-screen flex items-center justify-center" : "bg-[#D8E9EC] h-screen flex items-center justify-center") : "h-screen flex items-center justify-center"}>
         <div className='bg-white w-[500px] m-auto rounded-[25px] bg-opacity-70 shadow-md text-black pt-10 pb-10 '>
             <div className='flex flex-col items-center justify-center'>
                 <img src={logoImg} alt="" className='max-w-[250px] pb-5'/>
-                <div className="">
+                <div className="relative mb-3">
                     <input 
                     type="text" 
+                    id="floatingInput"
                     value={location} 
                     onChange={event => setLocation(event.target.value)}
                     onKeyPress={searchLocation}
                     placeholder='Pesquisar cidade'
-                    className='border-2 font-mono border-gray-300 bg-white h-10  rounded-lg text-sm text-center focus:outline-none'
+                    className='peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+                    /* className='border-2 font-mono border-gray-300 bg-white h-10  rounded-lg text-sm text-center focus:outline-none' */
                     />
+                    <label
+                     for="floatingInput"
+                    class="pointer-events-none text-center absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                    >Pesquisar cidade</label
+                    >
                 </div>
                 <div className='top'>
                 <div className="flex flex-col items-center justify-center mt-3">
@@ -57,7 +65,7 @@ const WeatherApp = () => {
                     </div>
                     
                     <div className="flex flex-col justify-center items-center mt-3">
-                        <div>
+                        <div className=''>
                             {weather.current ? <p className='font-mono'>{weather.current.condition.text}</p> : null}
                         </div>
                         <div>
